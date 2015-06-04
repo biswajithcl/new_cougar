3fc369acf18c91fd4e72ccc991692cf804a51209
Biswajit Roy <biswajit.r@hcl.com>
2015-06-04 17:39:24 +0530

    New Repository

---
+++ bootstrap_subtheme_new_repository_sub_3/template.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @file
+ * template.php
+ */
+function bootstrap_subtheme_for_cougar_2_preprocess_html(&$variables) {
+	$variables['classes_array'][] = "new_class_body_sub_theme_2";
+}
